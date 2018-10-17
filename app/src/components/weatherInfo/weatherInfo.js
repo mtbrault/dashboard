218e8bdf3fecaf280882d00f2a481c754dc8652e
cicorias <zakaria.laabid@epitech.eu>
2018-10-17 05:30:05 +0200

    Gesture weather done

--- app/src/components/weatherInfo/weatherInfo.js
+++ app/src/components/weatherInfo/weatherInfo.js
@@ -5,23 +5,51 @@ import SunImg from './weatherImages/sun.png';
 import WeatherImg from './weatherImages/weather.png';
 import ColdImg from './weatherImages/cold.png';
 
+const API_KEY = "403df07bdf873cdb79affc3a3afcc213";
+
 
 class WeatherInfo extends React.Component {
     constructor(props) {
-        super(props);
+        super(props);    
+}
+state = { 
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
+    getWeather = async (ville) => {
+        const country = ville;
+        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${country}&appid=${API_KEY}&units=metric`);
+        const data = await api_call.json();
+        this.setState({
+            temperature: Math.round(data.main.temp),
+            city: data.name,
+            country: data.sys.country,
+            humidity: data.main.humidity,
+            description: data.weather[0].description,
+            error: ""
+        })
+        console.log(this.state.temperature);
+    }
+
+    componentWillMount() {
+        this.getWeather(this.props.ville);
     }
     render() {
         return (
             <div>
                 <div className={Style.temperature}>
-                    <span>{this.props.temperature}
-                        <sup>°C</sup>
+                    <span>
+                        {this.state.temperature}<sup>°C</sup>
                     </span>
                     <img src={WeatherImg} />
                 </div>
                 <hr style={{ opacity: 0.5 }}></hr>
                 <div>
-                    <h2 className={Style.textVille}>{this.props.city}</h2>
+                    <h2 className={Style.textVille}>{this.state.city}</h2>
                 </div>
             </div>
         );
--- app/src/view/main/services/meteo.js
+++ app/src/view/main/services/meteo.js
@@ -5,51 +5,37 @@ import { Card, Row, Col, Avatar, Icon } from 'antd';
 import AddWidget from '../../../components/addWidget';
 import WeatherInfo from '../../../components/weatherInfo';
 
-const API_KEY = "403df07bdf873cdb79affc3a3afcc213";
-
 const services = {
   weather: {
-    city: ["Paris", "Lille", "Londre"]
+    city: ["Paris"]
   }
 };
+const arrWeather = services.weather.city;
 
 class Meteo extends React.PureComponent {
-  state = { 
-    temperature: undefined,
-    city: undefined,
-    country: undefined,
-    humidity: undefined,
-    description: undefined,
-    error: undefined
+  constructor(props) {
+    super(props);
+    this.state = {cityLength: arrWeather.length};
   }
-  getWeather = async (ville) => {
-    const country = ville;
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${country}&appid=${API_KEY}&units=metric`);
-    const data = await api_call.json();
-    console.log(data);
-    this.setState({
-      temperature: Math.round(data.main.temp),
-      city: data.name,
-      country: data.sys.country,
-      humidity: data.main.humidity,
-      description: data.weather[0].description,
-      error: ""
-    })
-  }
-
   render() {
     return (
       <Row gutter={16} style={{ marginLeft: "40px" }}>
-        <Col span={8}>
-          <Card
-            style={{ width: 300 }}
-            cover={<img alt="example" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Paris_-_Eiffelturm_und_Marsfeld2.jpg/1200px-Paris_-_Eiffelturm_und_Marsfeld2.jpg" width="298" height="180" />}
-            actions={[<Icon type="setting" />, <Icon type="delete" />]}
-          >
-          <WeatherInfo {...this.state} />
-          </Card>
-        </Col>
+        {arrWeather.map(ville => {
+          return (
+            <Col span={8} key={ville}>
+              <Card
+                style={{ width: 300 }}
+                cover={<img alt="example" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Paris_-_Eiffelturm_und_Marsfeld2.jpg/1200px-Paris_-_Eiffelturm_und_Marsfeld2.jpg" width="298" height="180" />}
+                actions={[<Icon type="setting" />, <Icon type="delete" />]}
+              >
+                <WeatherInfo ville={ville} />
+              </Card>
+            </Col>
+          );
+        })}
+        {this.state.cityLength < 3 &&
         <AddWidget />
+        }
       </Row>
     )
   }
