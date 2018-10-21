eb73a679814d3e3647662fd50ccddd1677eba383
cicorias <zakaria.laabid@epitech.eu>
2018-10-21 23:05:10 +0200

    Redux bug done

--- app/src/redux/store.js
+++ app/src/redux/store.js
@@ -5,7 +5,6 @@ import rootReducer from './reducers';
 const inititalState = {};
 
 const store = createStore(rootReducer, inititalState, 
-compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()));
+compose(applyMiddleware(thunk)));
 
 export default store;
